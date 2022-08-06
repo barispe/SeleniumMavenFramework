040986c350e4ff08c54830f763c6a7dddbea9c1e
barispe <perveroglubaris@gmail.com>
2022-08-07 00:31:01 +0300

    add ability to take screenshots after failed steps

--- src/test/java/stepDefinitions/base/Hooks.java
+++ src/test/java/stepDefinitions/base/Hooks.java
@@ -1,7 +1,14 @@
 package stepDefinitions.base;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.sql.Timestamp;
+
 import static driver.DriverFactory.cleanupDriver;
 import static driver.DriverFactory.getDriver;
 
@@ -11,6 +18,20 @@ public class Hooks {
     @Before
     public void setup(){
         getDriver();
+    }
+
+    @AfterStep
+
+    //taking screenshots of failed steps
+    public void captureExceptionImage(Scenario scenario){
+        if(scenario.isFailed()){
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            String timeMilliSeconds = Long.toString(timestamp.getTime());
+
+            byte[] screenshot = ((TakesScreenshot) getDriver())
+                    .getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png", timeMilliSeconds);
+        }
     }
 
     @After
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -7,7 +7,7 @@ Feature: WebDriver University - Login Page
 
 
   Scenario: Validate Successful Login
-    When I enter a username "webdriver"
+    When I enter a username webdriver
     And I enter a password webdriver123
     And I click on the login button
     Then I should be presented with the successful login message
