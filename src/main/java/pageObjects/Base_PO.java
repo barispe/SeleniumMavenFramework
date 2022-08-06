5960cb67d974cae9825ad4161d8c25dbf5476382
barispe <perveroglubaris@gmail.com>
2022-08-07 01:04:13 +0300

    add methods to base

--- src/main/java/pageObjects/Base_PO.java
+++ src/main/java/pageObjects/Base_PO.java
@@ -4,7 +4,7 @@ import driver.DriverFactory;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -31,5 +31,19 @@ public class Base_PO {
         WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
         //wait until element is to be clickable
         wait.until(ExpectedConditions.elementToBeClickable(by)).sendKeys((textToType));
-}
+    }
+    public void sendKeys(WebElement element, String textToType){
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).sendKeys((textToType));
+    }
+
+    public void waitForWebElementAndClick(By by){
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
+    }
+    public void waitForWebElementAndClick(WebElement element){
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
+
 }
