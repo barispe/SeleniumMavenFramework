baddfaef72bd179e8135226bc58209e590233ab6
barispe <perveroglubaris@gmail.com>
2022-08-07 18:26:24 +0300

    add testng and surefire for jenkins implementation

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -6,7 +6,7 @@ import org.testng.annotations.DataProvider;
 
 
 @CucumberOptions(features = {"classpath:features"},glue = {"stepDefinitions"},
-                tags = "@login",monochrome = true,dryRun = false,
+                tags = "",monochrome = true,dryRun = false,
                 plugin = {"pretty", "html:target/cucumber.html","json:target/cucumber.json"})
 
 public class MainRunner extends AbstractTestNGCucumberTests {
